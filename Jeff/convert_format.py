1812ee33d2d86d928fcb38d7d0be9e605d5fe101
Jeffery Wang <jrwang@ucsd.edu>
2015-11-23 18:00:59 -0800

    Added file to convert to pickle format

---
+++ Jeff/convert_format.py
@@ -0,0 +1,18 @@
+import csv
+import pickle
+fields = map(eval, open("fields").read().split(','))
+print "Enter file to convert: ", 
+f = open(raw_input())
+reader = csv.DictReader(f, fields)
+sample = list(reader)[1:]
+for line in sample:
+    for key in line:
+        try:
+            line[key] = eval(line[key])
+        except:
+            pass
+f.close()
+print "Enter pickle file name to write: ",
+f = open(raw_input(), 'w')
+pickle.dump(sample, f, 2)
+f.close()
